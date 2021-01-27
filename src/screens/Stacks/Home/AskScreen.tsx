c81b024dad939a7968f282ff7d326f5652305d0d
Paul Abel Sanchez <psanchez@oriontek.do>
2021-01-27 16:00:40 -0400

    preguntas y select

--- src/screens/Stacks/Home/AskScreen.tsx
+++ src/screens/Stacks/Home/AskScreen.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect,useState } from "react"
-import { Button, Text, View } from "react-native"
+import React, { useEffect, useState } from "react"
+import { Button, Dimensions, Platform, SafeAreaView, StyleSheet, Text, View } from "react-native"
+import { FlatList } from "react-native-gesture-handler";
 import Storage from "../../../constants/Storage";
 import { ProfileScreenNavigationProp, ProfileScreenRouteProp } from "../../../RouteStack";
+import PreguntaComponent from '../../../components/PreguntaComponent';
+import Color from '../../../constants/Colors';
 
 type Props = {
   route: ProfileScreenRouteProp;
   navigation: ProfileScreenNavigationProp;
 };
-
+const deviceWidth = Dimensions.get('window').width;
+const deviceHeight = Dimensions.get('window').height;
 const AskScreen = (props: Props) => {
   const [listado, setListado] = useState([]);
   useEffect(() => {
@@ -18,15 +22,40 @@ const AskScreen = (props: Props) => {
     console.log(listado);
   }, []);
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Ask</Text>
+    
+    <SafeAreaView style={styles.content}>
+      <Text>Encuesta</Text>
+      {/* <PreguntaComponent data={listado[0]} /> */}
+      <FlatList style={styles.list}
+        data={listado}
+        initialNumToRender={7}
+        onEndReached={() => console.log('el final')}
+        renderItem={({ item, index }) =>
+          <PreguntaComponent data={item} />
+        }
+        keyExtractor={(item, index) => index.toString()}
+      />
       <Button
         onPress={() => props.navigation.navigate('Home')}
         // onPress={() => console.log(listado[0].opciones)}
         title="Go to home"
       />
-    </View>
+    </SafeAreaView>
   )
 }
-
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // alignSelf:'center'
+    // backgroundColor: Color.primary
+  },
+  list: {
+    width:deviceWidth,
+    // height:deviceHeight,
+    // marginBottom: Platform.OS === 'android' ? 30 : 1,
+    // backgroundColor:Color.danger
+  }
+})
 export default AskScreen;
